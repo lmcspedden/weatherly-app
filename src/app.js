e02e61a348ee22e5d0ce20bee5720ae1297b3fab
lmcspedden <louisa.mcspedden@safetyculture.io>
2022-03-13 17:27:16 +1100

    Added forecast API

--- src/app.js
+++ src/app.js
@@ -22,6 +22,41 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+            <img 
+            src="http://openweathermap.org/img/wn/03n@2x.png" 
+            alt="" 
+            width="42" />
+         <div class="weather-forecast-temperature">
+            <span class="weather-forecast-temperature-maximum">18°</span>
+            <span class="weather-forecast-temperature-minimum"> 12°</span>
+        </div>
+    </div>
+    `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "974a674da0c7eec22aa4769f587dfc4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude={part}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -44,6 +79,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
